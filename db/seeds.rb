d7bde566a91c9d07c99d08dd2f9d7da0c5e6a443
Ghazanfar Ali <gak_92@hotmail.com>
2022-11-22 08:30:10 +0500

    Add data into seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+# Uncomment following line before run rails db:seed
+
+# user1 = User.create(name: "test1")
+# user2 = User.create(name: "test2")
+
+# category1 = Category.create(name: "Laptop", icon: "https://images.unsplash.com/photo-1661961111184-11317b40adb2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80", author: user1)
+# category2 = Category.create(name: "Car", icon: "https://images.unsplash.com/photo-1669069511294-e21722c7c5f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80", author: user2)
+
+# payment1 = Payment.create(name: "HP", amount: 250, author: user1, category: category1)
+# payment2 = Payment.create(name: "BMW", amount: 3500, author: user2, category: category2)
