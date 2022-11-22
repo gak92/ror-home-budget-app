acf7b9a914402ac89f8496be281d3e64df37ca47
Ghazanfar Ali <gak_92@hotmail.com>
2022-11-22 08:30:38 +0500

    Update routes for users categories and payments

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "users#index"
+  
+  resources :users
+  resources :categories do
+    resources :payments
+  end
 
   # Defines the root path route ("/")
   # root "articles#index"
