a05daf09ad1c4f9da952a59f3306ccd0416deca1
Ghazanfar Ali <gak_92@hotmail.com>
2022-11-23 18:38:48 +0500

    Fix linters errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :categories, class_name: 'Category', foreign_key: 'author_id', dependent: :destroy
   has_many :payments, foreign_key: 'author_id', dependent: :destroy
 
