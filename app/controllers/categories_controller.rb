8b698442bb31b8ebe646b73d3e65d5d9590d1e7a
Ghazanfar Ali <gak_92@hotmail.com>
2022-11-24 00:12:15 +0500

    Add functionality to edit and delete category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,26 +1,38 @@
 class CategoriesController < ApplicationController
   def index
-    @user = current_user
-    @categories = @user.categories
+    @categories = current_user.categories
   end
 
   def show
-    @user = current_user
-    @category = @user.categories.find(params[:id])
+    @category = current_user.categories.find(params[:id])
     @payments = @category.payments.order('created_at DESC')
   end
 
   def new
-    @user = current_user
-    @category = @user.categories.new
+    @category = current_user.categories.new
   end
 
   def create
-    @user = current_user
-    @new_category = @user.categories.new(category_params)
+    @new_category = current_user.categories.new(category_params)
 
     render :new unless @new_category.save
 
+    redirect_to categories_path
+  end
+
+  def edit
+    @category = current_user.categories.find(params[:id])
+  end
+
+  def update
+    @category = current_user.categories.find(params[:id])
+    render :edit unless @category.update(category_params)
+    redirect_to categories_path
+  end
+
+  def destroy
+    @category = current_user.categories.find(params[:id])
+    @category.destroy
     redirect_to categories_path
   end
 
