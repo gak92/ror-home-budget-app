94bdd132fd68573dbc91123bfc6e7ac42fe509c2
Ghazanfar Ali <gak_92@hotmail.com>
2022-11-23 14:39:42 +0500

    Update functionality to create new category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,33 @@
 class CategoriesController < ApplicationController
   def index
     @user = current_user
+    @categories = @user.categories
   end
+
+  def show
+    @user = current_user
+    @category = @user.categories.find(params[:id])
+    @payments = @category.payments.order('created_at DESC')
+  end
+
+  def new
+    @user = current_user
+    @category = @user.categories.new
+  end
+
+  def create
+    @user = current_user
+    @new_category = @user.categories.new(category_params)
+
+    render :new unless @new_category.save
+
+    redirect_to categories_path
+  end
+  
+  private
+
+  def category_params
+    params.require(:categories).permit(:name, :icon, :author_id)
+  end
+
 end
