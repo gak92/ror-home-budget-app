c12e903c5d96c8b327962d00fcc787937d0c8597
Ghazanfar Ali <gak_92@hotmail.com>
2022-11-23 14:40:05 +0500

    Add functionality to create new payment

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,4 +1,25 @@
 class PaymentsController < ApplicationController
-  def index
+
+  def new
+    @user = current_user
+    @params = params
+    @category = @user.categories.find(params[:category_id])
+    @payment = @user.payments.new
   end
+
+  def create
+    @user = current_user
+    @category = @user.categories.find(params[:category_id])
+    @new_payment = @user.payments.new(name: params[:payment][:name], amount: params[:payment][:amount], category_id: params[:category_id])
+
+    render :new unless @new_payment.save
+    redirect_to category_path(@category.id)
+  end
+
+  private
+
+  def payment_params
+    params.require(:payment).permit(:name, :amount, :category_id)
+  end
+
 end
