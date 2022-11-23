44d1e52c44a67a72d6ecad51dd8f5ec121b5a1d0
Ghazanfar Ali <gak_92@hotmail.com>
2022-11-23 14:39:12 +0500

    Update category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-  has_many :payments, dependent: :destroy
+  has_many :payments, foreign_key: 'category_id', dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 250 }
   validates :icon, presence: true
